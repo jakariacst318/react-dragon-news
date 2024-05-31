85803ecc1b7d0bc64a11472da45e3a80f43dfce6
jakariacst318 <jsjakariacst@gmail.com>
2024-05-31 14:19:55 +0600

    left site Navbar

--- src/pages/Shared/LeftSiteNav/LeftSiteNav.jsx
+++ src/pages/Shared/LeftSiteNav/LeftSiteNav.jsx
@@ -1,8 +1,23 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftSiteNav = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect( () =>{
+        fetch('categories.json')
+        .then(res => res.json())
+        .then(data => setCategories(data))
+    },[])
     return (
-        <div>
-            <h2 className="text-2xl">Left Site Nav</h2>
+        <div className="space-y-5">
+            <h2 className="text-2xl font-semibold">All Categories</h2>
+            {
+                categories.map(category => <Link className="block ml-6 text-xl font-medium"
+                key={category.id}
+                to={`category ${category.id}`}
+                >{category.name}</Link>)
+            }
         </div>
     );
 };
